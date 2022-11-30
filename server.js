fb98a1d9e9a8c80f8936fca44cedb27c37fc44ca
Nikhil Kumar <a.p.nikhilkumar@gmail.com>
2022-11-30 23:38:36 +0530

    Changes in start script and port

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const MoviesRoute = require("./routes/movies");
-
-
+let port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(bodyparser.json());
 app.use("/movies", MoviesRoute);
 
-app.listen(8080);
+app.listen(port, () => console.log(`movie-app-server listening on port: ${port}`));
