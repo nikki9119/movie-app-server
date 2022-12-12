c32436c149d034257e8d29e0bedfbe906f3c2dbd
Nikhil Kumar <a.p.nikhilkumar@gmail.com>
2022-12-13 01:22:54 +0530

    Added login

--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,79 @@
 const express = require("express");
+const { sign } = require('jsonwebtoken');
+const { genSaltSync, compareSync, hashSync } = require('bcrypt');
+
+const Connection = require('../database/connection');
+
 const Router = express.Router();
-const connection = require('../database/connection');
 
 Router.put("/register", (req, res) => {
     const { username, password } = req.body;
-        if(username && password) {
-            connection.query(`INSERT INTO user(username, password) VALUES('${username}', '${password}')`, (err, rows, fields) => {
-                if(!err) {
-                    res.status(201).send({
-                        status: "Success",
-                        info: rows
+    const salt = genSaltSync(10);
+    saltedPassword = hashSync(password, salt);
+    if(username && password) {
+        Connection.query(`INSERT INTO user(username, password) VALUES('${username}', '${saltedPassword}')`, (err, rows, fields) => {
+            if(!err) {
+                res.status(201).send({
+                    status: "Success",
+                    info: rows
+                });
+            }
+            else {
+                if(err.code === "ER_DUP_ENTRY") {
+                    res.status(400).send({
+                        status: "Failure",
+                        message: `User '${username}' already exists`
                     });
                 }
-                else {
-                    if(err.code === "ER_DUP_ENTRY") {
-                        res.status(400).send({
-                            status: "Failure",
-                            message: `User '${username}' already exists`
-                        });
-                    }
-                    else{
-                        console.log(err);
-                        res.status(500).send({
-                            status: "Failure",
-                            message: err.sqlMessage
-                        });
-                    }
+                else{
+                    console.log(err);
+                    res.status(500).send({
+                        status: "Failure",
+                        message: err.sqlMessage
+                    });
                 }
-            });
-        }
+            }
+        });
     }
-);
+});
 
 Router.post("/login", (req, res) => {
-
+    const { username, password } = req.body;
+    if(username && password) {
+        Connection.query(`SELECT username, password from user where username='${username}'`, (err, rows, fields) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send({
+                    status: "Error",
+                    message: err
+                });
+            }
+            if(!rows.length) {
+                res.status(401).send({
+                    status: "Failure",
+                    message: "Invalid user or password"
+                })
+            }
+            let user = rows[0];
+            const result = compareSync(password, user.password);
+            if(result) {
+                res.password = undefined;
+                const jsontoken = sign({ username: user.username }, "qwe1234", {
+                    expiresIn: "30m"
+                });
+                res.status(200).send({
+                    status: "Success",
+                    message: "Login successful",
+                    token: jsontoken
+                });
+            } else {
+                res.status(401).send({
+                    status: "Failure",
+                    message: "Invalid user or password"
+                })
+            }
+        });
+    }
 });
 
 Router.post("/logout", (req, res) => {
